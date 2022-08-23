3f23bbe41c76f25a9b798f912efbc563d6bc9f16
Thisisravigupta <ravigupta35516@gmail.com>
2022-08-23 18:22:04 +0530

    addDigitalClock

---
+++ script.js
@@ -0,0 +1,49 @@
+let time = document.querySelector(".time");
+let dateTime = document.querySelector(".date_time");
+
+function updateClock() {
+  let now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  let day = now.getDay();
+  let date = now.getDate();
+  let month = now.getMonth();
+  let year = now.getFullYear();
+
+  var dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  var monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  hours = hours % 12;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  date = date < 10 ? "0" + date : date;
+
+  let period = hours > 12 ? "AM" : "PM";
+  time.innerHTML = `${hours} : ${minutes} : ${seconds} ${period}`;
+  dateTime.innerHTML =
+    dayNames[day] + ", " + monthNames[month] + " " + date + ", " + year;
+}
+updateClock();
+setInterval(updateClock, 1000);
